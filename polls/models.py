d4a1b949cf6c644847bd71ac99b770c4449e050f
Owen Morgan <owen.morgan95@gmail.com>
2022-08-04 23:59:20 -0400

    First commit

---
+++ polls/models.py
@@ -0,0 +1,26 @@
+"""
+Poll Models
+"""
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    """ Poll Question """
+    question_text = models.CharField(max_length=200, default='why')
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    """ Poll Response Choice """
+    # ForeignKey tells Django each Choice is related to a single Question.
+    question = models.ForeignKey(Question,
+            on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
