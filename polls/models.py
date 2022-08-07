1881ff878d1fc6c8ff10edcbafc23faa0ba43667
Owen Morgan <owen.morgan95@gmail.com>
2022-08-07 03:02:37 -0400

    Complete tutorial

--- polls/models.py
+++ polls/models.py
@@ -5,13 +5,19 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
     """ Poll Question """
-    question_text = models.CharField(max_length=200, default='why')
+    question_text = models.CharField(max_length=200, default='Why?')
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    @admin.display(
+            boolean=True,
+            ordering='pub_date',
+            description='Recent?'
+            )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
